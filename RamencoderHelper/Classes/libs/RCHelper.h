bd7f1e7128cb5742a0872135db81cefa08eb1cf1
Zitao <caoer115@gmail.com>
2012-11-02 22:46:35 -0500

    add global import header

---
+++ RamencoderHelper/Classes/libs/RCHelper.h
@@ -0,0 +1,19 @@
+//
+//  RCHelper.h
+//  RamencoderHelper
+//
+//  Created by Zitao Xiong on 11/2/12.
+//  Copyright (c) 2012 Nanaimostudio. All rights reserved.
+//
+
+#ifndef RamencoderHelper_RCHelper_h
+#define RamencoderHelper_RCHelper_h
+
+#ifdef __OBJC__
+
+#import "GCDSingleton.h"
+#import "RCUIKit+Extensions.h"
+
+#endif
+
+#endif
---
+++ RamencoderHelper/Classes/libs/UIKit+Extension/RCUIKit+Extensions.h
@@ -0,0 +1,14 @@
+//
+//  RCUIKit+Extensions.h
+//  RamencoderHelper
+//
+//  Created by Zitao Xiong on 11/2/12.
+//  Copyright (c) 2012 Nanaimostudio. All rights reserved.
+//
+
+#ifndef RamencoderHelper_RCUIKit_Extensions_h
+#define RamencoderHelper_RCUIKit_Extensions_h
+
+#import "UIViewController+Nib.h"
+
+#endif
